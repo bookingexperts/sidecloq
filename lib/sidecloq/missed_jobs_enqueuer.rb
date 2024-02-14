ea8671b044c11665d715be7dc6fb7e6c073a8b17
jurruh <jurrevriesen@gmail.com>
2024-02-14 15:14:01 +0100

    Use Sidekiq.redis_pool.with

--- lib/sidecloq/missed_jobs_enqueuer.rb
+++ lib/sidecloq/missed_jobs_enqueuer.rb
@@ -1,14 +1,12 @@
 module Sidecloq
   class MissedJobsEnqueuer
-    def redis
-      Sidekiq.redis_pool.checkout
-    end
-
     def enqueue_missed_jobs(schedule)
       schedule.job_specs.each do |item|
         name, spec = item
 
-        next if (last_enqueued_at(name) || Time.now.to_i) >= previous_time(spec['cron'])
+        last_time = last_enqueued_at(name)
+        next if last_time.nil? # Never enqueued before, probably a new job. Skip it.
+        next if last_time >= previous_time(spec['cron'])
 
         JobEnqueuer.new(spec).enqueue
         log_last_enqueued(name)
@@ -16,13 +14,13 @@ module Sidecloq
     end
 
     def log_last_enqueued(name)
-      redis.set("sidecloq_enqueued_last_at_#{name}", Time.now.to_i)
+      Sidekiq.redis_pool.with { |redis| redis.set("sidecloq_enqueued_last_at_#{name}", Time.now.to_i) }
     end
 
     private
 
     def last_enqueued_at(name)
-      redis.get("sidecloq_enqueued_last_at_#{name}")&.to_i
+      Sidekiq.redis_pool.with { |redis| redis.get("sidecloq_enqueued_last_at_#{name}")&.to_i }
     end
 
     def previous_time(cron)
